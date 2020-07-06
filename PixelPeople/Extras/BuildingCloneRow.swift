481c61b55338ca47372231d9f01cf66af38e30e0
Eric A Carroll <peacarroll@comcast.net>
2020-07-05 23:39:25 -0400

    Added BuildingInfoView and BuildingCloneRow

---
+++ PixelPeople/Extras/BuildingCloneRow.swift
@@ -0,0 +1,57 @@
+//
+//  BuildingListView.swift
+//  PixelPeople
+//
+//  Created by Eric Carroll on 3/15/20.
+//  Copyright © 2020 Eric Carroll. All rights reserved.
+//
+
+import SwiftUI
+
+struct BuildingCloneRow: View {
+    var building: Building
+    var profession: Profession
+    
+    let colors: [String : [Color]] = ["Administration": [Color.olive, Color.oliveYellow],
+                                      "Business": [Color.yellowGreen, Color.greenYellow],
+                                      "Creative": [Color.yellowGreen, Color.greenYellow],
+                                      "Entertainment": [Color.fluorescentOrange, Color.darkOrange],
+                                      "F&B": [Color.yellowGreen, Color.greenYellow],
+                                      "Science": [Color.yellowGreen, Color.greenYellow],
+                                      "Services": [Color.yellowGreen, Color.greenYellow],
+                                      "None": [Color.yellowGreen, Color.greenYellow],
+    ]
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(colors[building.category]![0])
+                .frame(width: 280, height: 60)
+            RoundedRectangle(cornerRadius: 10)
+                .fill(colors[building.category]![1])
+                .frame(width: 270, height: 50)
+            HStack {
+                Image(profession.image)
+                    .resizable()
+                    .frame(width: 32, height: 32)
+                    .offset(x: -40, y: 0)
+                Text(profession.name)
+                    .font(.title)
+                    .offset(x: 40, y: 0)
+            }
+        }
+    }
+}
+
+struct BuildingCloneRow_Previews: PreviewProvider {
+    static let buildings: [Building] = Bundle.main.decode([Building].self, from: "buildings.json")
+    static let building = buildings[100]
+    
+    static let professions: [Profession] = Bundle.main.decode([Profession].self, from: "professions.json")
+    static let profession = professions[100]
+    
+    static var previews: some View {
+        BuildingCloneRow(building: building, profession: profession)
+    }
+}
+
--- PixelPeople/Extras/BuildingInfoView.swift
+++ PixelPeople/Extras/BuildingInfoView.swift
@@ -5,56 +5,84 @@
 //  Created by Eric Carroll on 3/15/20.
 //  Copyright © 2020 Eric Carroll. All rights reserved.
 //
-/*
+
 import SwiftUI
 
 struct BuildingInfoView: View {
-    var landCost: Int
-    var coinSecond: Int
-    var workTime: Int
-    var level: Int
-    @State private var isBuilt = false
+    @Binding var isPresented: Bool
+    
+    var building: Building
+    
+    let colors: [String : [Color]] = ["Administration": [Color.olive, Color.oliveYellow],
+                                      "Business": [Color.yellowGreen, Color.greenYellow],
+                                      "Creative": [Color.yellowGreen, Color.greenYellow],
+                                      "Entertainment": [Color.fluorescentOrange, Color.darkOrange],
+                                      "F&B": [Color.yellowGreen, Color.greenYellow],
+                                      "Science": [Color.yellowGreen, Color.greenYellow],
+                                      "Services": [Color.yellowGreen, Color.greenYellow],
+                                      "None": [Color.yellowGreen, Color.greenYellow],
+    ]
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Toggle(isOn: $isBuilt) {
-                Text("Building Built?")
-            }
-            .padding([.trailing], 120)
-            .toggleStyle(CheckboxToggleStyle())
-            .onTapGesture {
-                self.isBuilt = true
-                
-            }
-            
-            HStack {
-                Text("Level: \(level)")
+        NavigationView {
+            ZStack {
+                Group {
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(colors[building.category]![0])
+                        .frame(width: 320, height: 520)
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.black)
+                        .frame(width: 304, height: 504)
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(colors[building.category]![1])
+                        .frame(width: 244, height: 354)
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(LinearGradient(gradient: Gradient(colors: [colors[building.category]![0], colors[building.category]![1]]), startPoint: UnitPoint(x: 100, y: 150), endPoint: UnitPoint(x: 100, y: 300)))
+                        .frame(width: 300, height: 500)
+                    Rectangle()
+                        .fill(colors[building.category]![0])
+                        .frame(width: 205, height: 35)
+                        .offset(x: 0, y: -243)
+                    Rectangle()
+                        .fill(Color.gray)
+                        .frame(width: 200, height: 30)
+                        .offset(x: 0, y: -243)
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.gray)
+                        .frame(width: 280, height: 400)
+                    ForEach(0..<building.jobs.count) {
+                        BuildingCloneRow(building: self.building, profession: self.building.jobs[$0])
+                            .offset(x: 0, y: -175 + $0 * 25)
+                    }
+                    
+                }
+                Text(building.name.uppercased())
+                    .foregroundColor(.white)
+                    .offset(x: 0, y: -241)
                 Button(action: {
-  //                  if self.building.level < 6 {
-  //                      self.building.level += 1
-  //                      self.level = String(self.building.level)
-                        print("Test")
-  //                  }
-                }, label: { Image(systemName: "plus.square") })
+                    self.isPresented = false
+                }) {
+                    Image(systemName: "clear.fill")
+                    .resizable()
+                        .frame(width: 32, height: 32)
+                        .background(Color.white)
+                        .foregroundColor(Color.red)
+                }
+                .offset(x: 130, y: -230)
             }
-            
-            Text("Land Cost: \(landCost)")
-            Text("Coins per Second: \(coinSecond)")
-            Text("Adjusted Work Time: \(workTime)")
-        }
-            .padding(.leading, 10)
-            .frame(width: 275, height: 160, alignment: .center)
-            .overlay(Rectangle().stroke(Color.black, lineWidth: 5))
-            .background(Color.lightBlue)
-            .padding(.top, 50)
+        }.offset(x: 0, y: -50)
+    }
+    
+    var aspectRatio: CGFloat {
+            let size = UIImage(named: building.image)?.size ?? CGSize(width: 1, height: 1)
+            return size.width/size.height
     }
 }
 
 struct BuildingInfoView_Previews: PreviewProvider {
     static let buildings: [Building] = Bundle.main.decode([Building].self, from: "buildings.json")
-    static let building = buildings[100]
     static var previews: some View {
-        BuildingInfoView(landCost: building.landCost, coinSecond: building.cps, workTime: building.adjustedWorkTime, level: building.level)
+        BuildingInfoView(isPresented: .constant(true), building: buildings[29])
     }
 }
-*/
+
